ffef8e49872f78994721d4b5c37f53668dc271af
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2014-07-08 12:43:51 +0700

    WiFi driver from Samsung OSRC SM-T310 KitKat NF4

--- drivers/net/wireless/bcmdhd/include/brcm_nl80211.h
+++ drivers/net/wireless/bcmdhd/include/brcm_nl80211.h
@@ -1,5 +1,5 @@
 /*
- * Dongle WL Header definitions
+ * Definitions for nl80211 testmode access to host driver
  *
  * Copyright (C) 1999-2014, Broadcom Corporation
  * 
@@ -21,20 +21,36 @@
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
  *
- * $Id: dngl_wlhdr.h 241182 2011-02-17 21:50:03Z $
+ * $Id: brcm_nl80211.h 454792 2014-02-11 20:40:19Z $
+ *
  */
 
-#ifndef _dngl_wlhdr_h_
-#define _dngl_wlhdr_h_
+#ifndef _brcm_nl80211_h_
+#define _brcm_nl80211_h_
 
-typedef struct wl_header {
-    uint8   type;           /* Header type */
-    uint8   version;        /* Header version */
-	int8	rssi;			/* RSSI */
-	uint8	pad;			/* Unused */
-} wl_header_t;
+struct bcm_nlmsg_hdr {
+	uint cmd;	/* common ioctl definition */
+	uint len;	/* attached buffer length */
+	uint offset;	/* user buffer offset */
+	uint set;	/* get or set request optional */
+	uint magic;	/* magic number for verification */
+};
 
-#define WL_HEADER_LEN   sizeof(wl_header_t)
-#define WL_HEADER_TYPE  0
-#define WL_HEADER_VER   1
-#endif /* _dngl_wlhdr_h_ */
+enum bcmnl_attrs {
+	BCM_NLATTR_UNSPEC,
+
+	BCM_NLATTR_LEN,
+	BCM_NLATTR_DATA,
+
+	__BCM_NLATTR_AFTER_LAST,
+	BCM_NLATTR_MAX = __BCM_NLATTR_AFTER_LAST - 1
+};
+
+struct nl_prv_data {
+	int err;			/* return result */
+	void *data;			/* ioctl return buffer pointer */
+	uint len;			/* ioctl return buffer length */
+	struct bcm_nlmsg_hdr *nlioc;	/* bcm_nlmsg_hdr header pointer */
+};
+
+#endif /* _brcm_nl80211_h_ */
